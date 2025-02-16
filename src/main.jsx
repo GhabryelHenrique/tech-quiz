645d1857de1985b620ca54e2dfa031cc38bf43ce
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2025-02-16 19:25:52 -0300

    preparando para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import GlobalStyles from './assets/styles/global'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import GlobalStyles from './assets/styles/global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <HashRouter>
+      <GlobalStyles />
+      <App />
+    </HashRouter>
   </React.StrictMode>
-)
+);
